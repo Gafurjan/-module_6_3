bf2fa7c8cf84b30da53ce4d7ac50828546b42c9b
Gafurjan <48514749+Gafurjan@users.noreply.github.com>
2024-10-04 14:18:47 +0300

    Add files via upload

---
+++ module_6_3..py
@@ -0,0 +1,32 @@
+class Horse:
+    def __init__(self, x_distance = 0, sound = 'Frrr'):
+        super().__init__()
+        self.x_distance = x_distance
+        self.sound = sound
+
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat' ):
+        super().__init__()
+        self.y_distance = y_distance
+        self.sound = sound
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+
